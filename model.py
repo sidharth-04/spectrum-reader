814c47be2946c5b09362fac3f13b1b82f7ede448
Sidharth Roy <68526404+sidharth-04@users.noreply.github.com>
2023-03-05 19:08:37 +0530

    Model Update

--- model.py
+++ model.py
@@ -4,23 +4,52 @@ from keras import layers
 import random
 import matplotlib.pyplot as plt
 
-train_features = np.load("processed/spectradata.npy")
-train_labels = np.load("processed/labeldata.npy")
-temp = list(zip(train_features, train_labels))
-random.shuffle(temp)
-train_features, train_labels = zip(*temp)
+dir_loc = '/content/drive/MyDrive/PhysicsIA/data/'
 
-def build_model():
+features = np.load(dir_loc+"spectradata.npy")
+labels = np.load(dir_loc+"labeldata.npy")
+temp = list(zip(features, labels))
+random.shuffle(temp)
+features, labels = zip(*temp)
+features, labels = np.array(features), np.array(labels)
+
+train_features = features[100:]
+train_labels = labels[100:]
+
+mean = train_features.mean()
+train_features -= mean
+std = train_features.std()
+train_features /= std
+
+test_features = features[:100]
+test_labels = labels[:100]
+
+test_features -= mean
+test_features /= std
+
+def build_model_overfitting():
     model = models.Sequential()
-    model.add(layers.Dense(64, activation='relu', input_shape=(train_features.shape[1])))
-    model.add(layers.Dense(64,activation='relu'))
+    model.add(layers.Dense(14, activation='relu', input_shape=(train_features.shape[1],)))
+    model.add(layers.Dense(14,activation='relu'))
+    model.add(layers.Dense(1))
     model.compile(optimizer='adam', loss='mse', metrics=['mae'])
     return model
 
 model = build_model()
-history = model.fit(train_features, train_labels, validation_split=0.2, epochs=10, batch_size=64)
-mae_history = history.history["val_mean_absolute_error"]
-plt.plot(range(1, len(mae_history)+1), mae_history)
+history = model.fit(
+    train_features,
+    train_labels,
+    validation_split=0.1,
+    epochs=60,
+    batch_size=64)
+
+val_mae_history = history.history["val_mae"]
+mae_history = history.history["mae"]
+plt.plot(range(10, len(mae_history)+1), mae_history[9:], 'b-', label="training error")
+plt.plot(range(10, len(val_mae_history)+1), val_mae_history[9:], 'r-',  label="validation error")
+leg = plt.legend()
 plt.xlabel("Epochs")
-plt.ylabel("Validation MAE")
+plt.ylabel("Error")
 plt.show()
+
+model.evaluate(test_features, test_labels)
